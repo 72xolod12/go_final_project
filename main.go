d3b58ad2dc852337d6818409126815d163c910e0
72xolod12 <ivashentsev.karaganda@gmail.com>
2026-04-17 07:49:48 +0000

    Cleanup

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"go_final_project/pkg/api"
@@ -114,4 +118,58 @@ func nextDateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte(nextDate))
+}
+func NextDate(now time.Time, dstart string, repeat string) (string, error) {
+
+	if repeat == "" {
+		return "", errors.New("правило повторения не указано")
+	}
+
+	start, err := time.Parse(dateFormat, dstart)
+	if err != nil {
+		return "", fmt.Errorf("некорректная дата начала: %v", err)
+	}
+
+	parts := strings.Split(repeat, " ")
+	rule := parts[0]
+
+	switch rule {
+	case "y":
+
+		res := start
+		for {
+			res = res.AddDate(1, 0, 0)
+
+			if res.After(now) {
+				return res.Format(dateFormat), nil
+			}
+		}
+
+	case "d":
+
+		if len(parts) < 2 {
+			return "", errors.New("не указано количество дней для правила d")
+		}
+
+		days, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return "", errors.New("некорректный формат дней")
+		}
+
+		if days > 400 {
+			return "", errors.New("превышен интервал в 400 дней")
+		}
+
+		res := start
+		for {
+			res = res.AddDate(0, 0, days)
+			if res.After(now) {
+				return res.Format(dateFormat), nil
+			}
+		}
+
+	default:
+
+		return "", fmt.Errorf("неподдерживаемый формат правила: %s", rule)
+	}
 }
